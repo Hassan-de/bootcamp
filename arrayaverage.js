a8e86cd94aff01b0cd1885056560c8d1be19990d
Hassan-de <hassan3119863001@gmail.com>
2023-02-01 17:42:23 +0500

    commit

---
+++ arrayaverage.js
@@ -0,0 +1,9 @@
+const arrayAverage = (numbers) => {
+    let sum = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum / numbers.length;
+  };
+  console.log(arrayAverage([1, 2, 3, 4, 5]));
+  
